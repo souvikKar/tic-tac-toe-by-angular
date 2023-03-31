978d8154b06129b28a643a40c636b81ed4795fd5
Souvik Kar <souvikkar0101@gmail.com>
2023-03-31 23:31:13 +0530

    tic-tac-toe

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,12 @@
+import { GameService } from './../game.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.css']
+})
+export class BoardComponent {
+
+  constructor(public gameService: GameService) { }
+}
